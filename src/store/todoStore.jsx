7b77ad190c121037708cbcb1ca0bfc8d9d4c201f
Elina Eriksson Hult <elinaerikssonhult@gmail.com>
2024-11-22 21:55:39 +0100

    Error fix x3

--- src/store/todoStore.jsx
+++ src/store/todoStore.jsx
@@ -1,12 +1,16 @@
 // Create state management with Zustand 
 import { create } from "zustand";
 
-export const useTodoStore = create((set) => ({
+export const useTodoStore = create((set, get) => ({
   todos: [],
+  
+  // Add a new todo
   addTodo: (text) =>
     set((state) => ({
       todos: [...state.todos, { id: Date.now(), text, completed: false }],
     })),
+  
+  // Toggle todo completion status
   toggleTodo: (id) =>
     set((state) => ({
       todos: state.todos.map((todo) => 
@@ -14,16 +18,17 @@ export const useTodoStore = create((set) => ({
       ),
     })),
 
-    //Delete to-do
-    deleteTodo: (id) =>
-      set ((state) => ({
-        todos: state.todos.filter((todo) => todo.id !== id),
-      })),
+  // Delete a todo
+  deleteTodo: (id) =>
+    set((state) => ({
+      todos: state.todos.filter((todo) => todo.id !== id),
+    })),
 
-      //Task counts
-      getIncompleteCount: () =>
-        get().todos.filter((todo) => !todo.completed).length,
-    
-      getCompletedCount: () =>
-        get().todos.filter((todo) => todo.completed).length,
+  // Get count of incomplete todos
+  getIncompleteCount: () => 
+    get().todos.filter((todo) => !todo.completed).length,
+  
+  // Get count of completed todos
+  getCompletedCount: () => 
+    get().todos.filter((todo) => todo.completed).length,
 }));

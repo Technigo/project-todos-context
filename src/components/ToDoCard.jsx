68879835b6ec6d0b00aa8667a454a4788a7aefc5
Gitte Beckmann <gittebeckmann@hotmail.com>
2024-11-26 14:31:14 +0100

    - deleted one button and step to choose

--- src/components/ToDoCard.jsx
+++ src/components/ToDoCard.jsx
@@ -2,46 +2,41 @@ import { useToDoStore } from "../stores/useToDoStore";
 import { ToDoSubmit } from "./ToDoSubmit";
 import { Checkbox } from "./CheckBox";
 import "./ToDoCard.css";
-import { AddTaskButton } from "../ui/AddTaskButton";
 import { BinButton } from "../ui/BinButton";
 import { Headline2 } from "../ui/Typography";
 import noToDoImg from "../assets/noToDoImg.png";
 
 export const ToDoCard = () => {
-  const { todos, toggleTodo, showForm } = useToDoStore();
+  const { todos, toggleTodo } = useToDoStore();
 
   return (
     <>
-      <div className="to-do-box">
-        <AddTaskButton />
-        {showForm && <ToDoSubmit />}
-
-        <div className="todo-card-container">
-          {todos.length === 0 ? (
-            <div className="no-projects">
-              <div className="notes-image-container">
-                <img src={noToDoImg} alt="No Projects" className="notes-image" />
-                <Headline2>Oops, nothing here!</Headline2>
-                <p>Start by typing something to add a new task</p>
-              </div>
+      <div className="todo-card-container">
+        <ToDoSubmit />
+        {todos.length === 0 ? (
+          <div className="no-projects">
+            <div className="notes-image-container">
+              <img src={noToDoImg} alt="No Projects" className="notes-image" />
+              <Headline2>Oops, nothing here!</Headline2>
+              <p>Start by typing something to add a new task</p>
             </div>
-          ) : todos.map((todo) => (
-            <div key={todo.id} className="todo-item">
-              <div className="text-wrapper">
-                <span
-                  className={`todo-text ${todo.completed ? "completed" : ""}`}
-                  onClick={() => toggleTodo(todo.id)}
-                >
-                  {todo.text}
-                </span>
-              </div>
-              <div className="card-button-container">
-                <Checkbox todo={todo} />
-                <BinButton todoId={todo.id} />
-              </div>
+          </div>
+        ) : todos.map((todo) => (
+          <div key={todo.id} className="todo-item">
+            <div className="text-wrapper">
+              <span
+                className={`todo-text ${todo.completed ? "completed" : ""}`}
+                onClick={() => toggleTodo(todo.id)}
+              >
+                {todo.text}
+              </span>
             </div>
-          ))}
-        </div>
+            <div className="card-button-container">
+              <Checkbox todo={todo} />
+              <BinButton todoId={todo.id} />
+            </div>
+          </div>
+        ))}
       </div>
     </>
   );

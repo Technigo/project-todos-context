60b5029be9e5ea3422dfce59f3c22261781cf328
Gitte Beckmann <gittebeckmann@hotmail.com>
2024-11-28 10:43:13 +0100

    - converted files from jsx to tsx

---
+++ src/main.tsx
@@ -0,0 +1,23 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import { App } from "./App.jsx";
+// import "./index.css";
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./App";
+import "./index.css";
+
+const rootElement = document.getElementById("root") as HTMLElement;
+
+ReactDOM.createRoot(rootElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
